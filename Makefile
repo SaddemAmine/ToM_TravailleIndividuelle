972915994699c6f7dc55ab1c596138a7f3ee74d3
SaddemAmine <naceuramine.saddem@esprit.tn>
2019-03-29 16:59:00 +0100

    tâches indiv terminées

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@
 prog: main.c
 	gcc main.c -o prog -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer -g
 
+prog2: scroll.c
+	gcc scroll.c -o prog2 -lSDL -lSDL_image -lSDL_ttf -lSDL_mixer -g
+
 clean:
 	rm *.o prog -f
---
+++ scroll.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+void main(){
+    int x = 0;
+    SDL_Surface* img = NULL; SDL_Surface* scr = NULL; SDL_Event event;
+    scr = SDL_SetVideoMode(300,300,32,SDL_DOUBLEBUF|SDL_HWSURFACE);
+    img = IMG_Load("img2.png");
+
+    SDL_Rect pos;
+    pos.x = 0; pos.y = 0;
+
+    if(SDL_Init(SDL_INIT_VIDEO)!=0){
+        printf("Error: %s\n",SDL_GetError());
+        return;
+    }
+
+    while(1){
+        SDL_Delay(5);
+        SDL_BlitSurface(img,NULL,scr,&pos);
+        SDL_Flip(scr);
+        if(x>-1000)
+            x--;
+        else
+            x = 0; 
+        pos.x = x;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.sym == SDLK_SPACE)
+                        return;
+                break;
+            }
+        }
+        printf("%d --\n",pos.x);
+    }
+    
+}
